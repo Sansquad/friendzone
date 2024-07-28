5ec5580c647fd2d0a1b5cd9349b04720effe9b7a
Seungje <davidseong8914@gmail.com>
2024-07-28 16:00:16 +0900

    GoogleMaps API testing page

---
+++ lib/pages/google_map_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+
+class GoogleMapPage extends StatefulWidget {
+  const GoogleMapPage({super.key});
+
+  @override
+  State<GoogleMapPage> createState() => _GoogleMapPageState();
+}
+
+class _GoogleMapPageState extends State<GoogleMapPage> {
+  static const googlePlex = LatLng(37.4223, -122.0848);
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: googlePlex,
+          zoom:13,
+        ),
+        markers: {
+          const Marker(
+            markerId: MarkerId('SourceLocation'),
+            icon:BitmapDescriptor.defaultMarker,
+            position: googlePlex,
+            ),
+        },
+     )
+  );
+}
