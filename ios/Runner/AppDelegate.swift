ae667142a96d1677bd11c85c16103c55d44975c6
Seungje <davidseong8914@gmail.com>
2024-08-04 16:06:49 +0900

    attempt develop google authenticaltion -> failed

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,3 +1,4 @@
+// What I originally had
 import Flutter
 import UIKit
 
@@ -11,3 +12,40 @@ import UIKit
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
+
+// provided by Firebase
+// import UIKit
+// import FirebaseCore
+
+
+// @UIApplicationMain
+// class AppDelegate: UIResponder, UIApplicationDelegate {
+
+//   var window: UIWindow?
+
+//   func application(_ application: UIApplication,
+//     didFinishLaunchingWithOptions launchOptions:
+//       [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+//     FirebaseApp.configure()
+
+//     return true
+//   }
+// }
+
+// Chat GPT's suggestion/ combination
+// import UIKit
+// import Flutter
+// import FirebaseCore
+
+// @UIApplicationMain
+// @objc class AppDelegate: FlutterAppDelegate {
+//   override func application(
+//     _ application: UIApplication,
+//     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//   ) -> Bool {
+//     FirebaseApp.configure()
+//     GeneratedPluginRegistrant.register(with: self)
+//     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//   }
+// }
+
