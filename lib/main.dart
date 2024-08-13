ecee9de08623d1ae965d418ee6faf3a2de84f4ff
Seungje <davidseong8914@gmail.com>
2024-08-13 13:37:33 +0900

    merge with local feature/welcome

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:friendzone/firebase_options.dart'; 
+// import 'package:friendzone/firebase_options.dart'; 
 import 'package:flutter/material.dart';
 
 import 'config/firebase_options.dart';
