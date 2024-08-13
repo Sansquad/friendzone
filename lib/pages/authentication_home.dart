d08fe3a64414ce62949f7469c2c94ac25aad4c27
Seungje <davidseong8914@gmail.com>
2024-08-13 14:40:46 +0900

    add darkmode lightmode feature to authentication_home.dart

--- lib/pages/authentication_home.dart
+++ lib/pages/authentication_home.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class HomePage extends StatelessWidget {
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface, // Use the theme's surface color
       body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background_blue.png'), // Ensure this path is correct
-            fit: BoxFit.cover,
-          ),
-        ),
+
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
@@ -18,20 +23,29 @@ class HomePage extends StatelessWidget {
 
               const SizedBox(height: 50),
               // Logo
-              Image.asset(
-                'assets/icons/logo_temp.png', // Ensure the image exists in this path
+              // Image.asset(
+              //   'assets/icons/logo_temp.png', // Ensure the image exists in this path
+              //   height: 150,
+              // ),
+              SvgPicture.asset(
+                'assets/icons/auth_logo.svg', // Ensure the image exists in this path
                 height: 150,
+                colorFilter: ColorFilter.mode(
+                  Theme.of(context).colorScheme.inverseSurface,
+                  BlendMode.srcIn,
+                ),
               ),
-              const SizedBox(height: 0),
+              const SizedBox(height: 20),
               // App Name
               RichText(
-                text: const TextSpan(
+                text: TextSpan(
                   children: [
                     TextSpan(
                       text: 'Friend',
                       style: TextStyle(
                         fontFamily: 'TurretRoad',
-                        color: Colors.black,
+                        color: Theme.of(context).colorScheme.inverseSurface,
+                        
                         fontSize: 55,
                         fontWeight: FontWeight.bold,
                       ),
@@ -40,7 +54,7 @@ class HomePage extends StatelessWidget {
                       text: 'zone',
                       style: TextStyle(
                         fontFamily: 'TurretRoad',
-                        color: Colors.white,
+                        color: Theme.of(context).colorScheme.primary,
                         fontSize: 55,
                         fontWeight: FontWeight.bold,
                       ),
@@ -48,7 +62,7 @@ class HomePage extends StatelessWidget {
                   ],
                 ),
               ),
-              const SizedBox(height: 60), // Increased space between text and buttons
+              const SizedBox(height: 40), // Increased space between text and buttons
               SizedBox(
                 width: 288,
                 height: 48,
@@ -58,15 +72,19 @@ class HomePage extends StatelessWidget {
                     Navigator.pushNamed(context, '/signin'); // Navigate to sign-in page
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    foregroundColor: Theme.of(context).colorScheme.surface,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(15), // Border radius
                     ),
                   ),
                   child: const Text(
                     'Sign In',
-                    style: TextStyle(fontSize: 18),
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontFamily: 'BigShouldersDisplay',
+                      fontWeight: FontWeight.w800,
+                      ),
                   ),
                 ),
               ),
@@ -80,15 +98,19 @@ class HomePage extends StatelessWidget {
                     Navigator.pushNamed(context, '/getstarted'); // Navigate to get started page
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    foregroundColor: Colors.black,
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    foregroundColor: Theme.of(context).colorScheme.surface,
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(15), // Border radius
                     ),
                   ),
                   child: const Text(
                     'Create Account',
-                    style: TextStyle(fontSize: 18),
+                    style: TextStyle(
+                      fontSize: 23,
+                      fontFamily: 'BigShouldersDisplay',
+                      fontWeight: FontWeight.w800,
+                      ),
                   ),
                 ),
               ),
