9d8789b47f668b7424c42d05f5ff57c9830146dd
leesj092 <slee887@wisc.edu>
2024-08-19 19:14:11 +0900

    Restored onTap capability in authentication_start

--- lib/pages/authentication_start.dart
+++ lib/pages/authentication_start.dart
@@ -1,19 +1,16 @@
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:friendzone/services/auth/firebase_auth_services.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:friendzone/pages/authentication_start.dart';
 import '../components/form_container_widget.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-
-
 class GetStartedPage extends StatefulWidget {
-  const GetStartedPage({Key? key}) : super(key: key);
+  final void Function()? onTap;
+  const GetStartedPage({super.key, required this.onTap});
 
   @override
   _GetStartedPageState createState() => _GetStartedPageState();
@@ -22,17 +19,16 @@ class GetStartedPage extends StatefulWidget {
 class _GetStartedPageState extends State<GetStartedPage> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseAuthService _firebaseAuth = FirebaseAuthService();
-  
+
   TextEditingController _usernameController = TextEditingController();
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
-  
+
   ValueNotifier<bool> _isPasswordInvalid = ValueNotifier(false);
   ValueNotifier<bool> _isPasswordValid = ValueNotifier(false);
 
   User? _user;
-  
-  
+
   // Check if password is valid (> 8 characters)
   @override
   void initState() {
@@ -45,7 +41,6 @@ class _GetStartedPageState extends State<GetStartedPage> {
     });
   }
 
-
   void _validatePassword() {
     final password = _passwordController.text;
     if (password.length >= 8) {
@@ -57,18 +52,16 @@ class _GetStartedPageState extends State<GetStartedPage> {
     }
   }
 
-  
   @override
-    void dispose() {
-      _usernameController.dispose();
-      _emailController.dispose();
-      _passwordController.dispose();
+  void dispose() {
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
 
-      _isPasswordInvalid.dispose();
-      _isPasswordValid.dispose();
-      super.dispose();
-    }
-
+    _isPasswordInvalid.dispose();
+    _isPasswordValid.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -77,7 +70,7 @@ class _GetStartedPageState extends State<GetStartedPage> {
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () {
-                Navigator.pushNamed(context, '/homepage');
+            Navigator.pop(context);
           },
         ),
         backgroundColor: Theme.of(context).colorScheme.surface,
@@ -96,7 +89,8 @@ class _GetStartedPageState extends State<GetStartedPage> {
                 Align(
                   alignment: Alignment.topLeft,
                   child: Padding(
-                    padding: const EdgeInsets.fromLTRB(36, 0, 0, 20), // Adjust padding for positioning
+                    padding: const EdgeInsets.fromLTRB(
+                        36, 0, 0, 20), // Adjust padding for positioning
                     child: RichText(
                       text: TextSpan(
                         children: [
@@ -106,7 +100,8 @@ class _GetStartedPageState extends State<GetStartedPage> {
                               fontFamily: 'BigShouldersText',
                               fontSize: 30,
                               fontWeight: FontWeight.bold,
-                              color: Theme.of(context).colorScheme.inverseSurface,
+                              color:
+                                  Theme.of(context).colorScheme.inverseSurface,
                             ),
                           ),
                           TextSpan(
@@ -128,7 +123,7 @@ class _GetStartedPageState extends State<GetStartedPage> {
                   controller: _usernameController,
                   hintText: 'Username',
                   isPasswordField: false,
-                  ),
+                ),
                 SizedBox(height: 20),
 
                 FormContainerWidget(
@@ -158,7 +153,9 @@ class _GetStartedPageState extends State<GetStartedPage> {
                             fontFamily: 'BigShouldersDisplay',
                             fontSize: 17,
                             fontWeight: FontWeight.w500,
-                            color: isPasswordInvalid ? Colors.red : Color(0xFF818080),
+                            color: isPasswordInvalid
+                                ? Colors.red
+                                : Color(0xFF818080),
                           ),
                         );
                       },
@@ -175,15 +172,16 @@ class _GetStartedPageState extends State<GetStartedPage> {
                       child: ElevatedButton(
                         onPressed: isPasswordValid
                             ? () {
-                              _signUp();
-                              // Navigator.pushNamed(context, '/contentlayout');
+                                _signUp();
+                                // Navigator.pushNamed(context, '/contentlayout');
                               }
                             : () {
                                 _isPasswordInvalid.value = true;
                                 // Optional: Implement buzzing effect here
                               },
                         style: ElevatedButton.styleFrom(
-                          backgroundColor: Theme.of(context).colorScheme.primary,
+                          backgroundColor:
+                              Theme.of(context).colorScheme.primary,
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(15),
                           ),
@@ -256,7 +254,7 @@ class _GetStartedPageState extends State<GetStartedPage> {
                 ),
                 SizedBox(height: 20),
 
-                  Row(
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
@@ -269,29 +267,25 @@ class _GetStartedPageState extends State<GetStartedPage> {
                       ),
                     ),
                     GestureDetector(
-                      // onTap: widget.showSignInPage,
-                      onTap: () {
-                        Navigator.pushNamed(context, '/signin');
-                      },
-                  child: Text(
+                      onTap: widget.onTap,
+                      child: Text(
                         'Sign In',
                         style: TextStyle(
                           fontFamily: 'BigShouldersDisplay',
                           fontSize: 17,
                           fontWeight: FontWeight.w700,
                           color: Theme.of(context).colorScheme.primary,
+                        ),
                       ),
                     ),
-                    ),
                   ],
-),
+                ),
                 SizedBox(height: 90),
-
               ],
+            ),
           ),
         ),
       ),
-    ),
     );
   }
 
@@ -310,7 +304,8 @@ class _GetStartedPageState extends State<GetStartedPage> {
         // The user canceled the sign-in
         return;
       }
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
       final OAuthCredential credential = GoogleAuthProvider.credential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
@@ -318,31 +313,38 @@ class _GetStartedPageState extends State<GetStartedPage> {
 
       // Sign in to Firebase with the Google [UserCredential]
       // final UserCredential userCredential = await _auth.signInWithCredential(credential);
-      UserCredential userCredential = await _auth.signInWithCredential(credential);
+      UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
       // _user = userCredential.user;
       User? user = userCredential.user;
 
       // setState(() {});
 
-    if (user != null) {
-      DocumentSnapshot userDoc = await FirebaseFirestore.instance.collection('users').doc(user.uid).get();
-      if (!userDoc.exists) {
-        await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
-          'username': user.displayName,
-          'email': user.email,
-          'createdAt': Timestamp.now(),
-        });
-        Navigator.pushNamed(context, '/createyourprofile');
-      } else {
-        Navigator.pushNamed(context, '/contentlayout');
+      if (user != null) {
+        DocumentSnapshot userDoc = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(user.uid)
+            .get();
+        if (!userDoc.exists) {
+          await FirebaseFirestore.instance
+              .collection('users')
+              .doc(user.uid)
+              .set({
+            'username': user.displayName,
+            'email': user.email,
+            'createdAt': Timestamp.now(),
+          });
+          Navigator.pushNamed(context, '/createyourprofile');
+        } else {
+          Navigator.pushNamed(context, '/contentlayout');
+        }
       }
+    } catch (error) {
+      print('Error during Google Sign-In: $error');
     }
-  } catch (error) {
-    print('Error during Google Sign-In: $error');
   }
-}
 
-  void _signUp() async{
+  void _signUp() async {
     String username = _usernameController.text;
     String email = _emailController.text;
     String password = _passwordController.text;
@@ -356,10 +358,10 @@ class _GetStartedPageState extends State<GetStartedPage> {
     try {
       // Check if the username exists
       QuerySnapshot usernameQuery = await FirebaseFirestore.instance
-        .collection('users')
-        .where('username', isEqualTo: username)
-        .limit(1)
-        .get();
+          .collection('users')
+          .where('username', isEqualTo: username)
+          .limit(1)
+          .get();
 
       if (usernameQuery.docs.isNotEmpty) {
         // Username is taken
@@ -370,7 +372,7 @@ class _GetStartedPageState extends State<GetStartedPage> {
         ));
         return;
       }
-          
+
       // Check if the email already exists
       QuerySnapshot emailQuery = await FirebaseFirestore.instance
           .collection('users')
@@ -389,10 +391,10 @@ class _GetStartedPageState extends State<GetStartedPage> {
       }
 
       // If both are available -> create the user
-      User? user = await _firebaseAuth.signUpWithEmailAndPassword(email, password);
+      User? user =
+          await _firebaseAuth.signUpWithEmailAndPassword(email, password);
 
       if (user != null) {
-
         // 만약 사용자의 추가적인 정보를 받고 싶다면 추가하기
         // 'users' collection에 저장됨
         await FirebaseFirestore.instance.collection('users').doc(user.uid).set({
@@ -406,7 +408,7 @@ class _GetStartedPageState extends State<GetStartedPage> {
         if (mounted) {
           // Navigator.pushNamed(context, '/contentlayout');
           Navigator.pushNamed(context, '/verifyemail');
-          }
+        }
       } else {
         print('Sign up failed');
       }
@@ -425,7 +427,8 @@ class SocialButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
 
-  const SocialButton({required this.assetPath, required this.text, required this.onPressed});
+  const SocialButton(
+      {required this.assetPath, required this.text, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
@@ -453,14 +456,13 @@ class SocialButton extends StatelessWidget {
               ),
             if (assetPath.endsWith('.svg'))
               SvgPicture.asset(
-                  assetPath, // Ensure the image exists in this path
-                  height: 24,
-                  colorFilter: ColorFilter.mode(
-                    Theme.of(context).colorScheme.inverseSurface,
-                    BlendMode.srcIn,
-                  ),
+                assetPath, // Ensure the image exists in this path
+                height: 24,
+                colorFilter: ColorFilter.mode(
+                  Theme.of(context).colorScheme.inverseSurface,
+                  BlendMode.srcIn,
                 ),
-
+              ),
             SizedBox(width: 0),
             Expanded(
               child: Center(
